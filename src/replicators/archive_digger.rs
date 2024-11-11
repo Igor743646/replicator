525c58c2785c1bd03071b4e273e7c230215fc0d4
Igor Glushatov <igor_743646@mail.ru>
2024-11-11 14:01:54 +0300

    Archive digger update

--- src/replicators/archive_digger.rs
+++ src/replicators/archive_digger.rs
@@ -1,4 +1,5 @@
 use std::collections::VecDeque;
+use std::ffi::{OsStr, OsString};
 use std::path::PathBuf;
 use log::{info, trace, warn};
 
@@ -7,24 +8,26 @@ use crate::common::OLRErrorCode::*;
 
 trait ArchiveDigger {
     fn get_parsers_queue(&self) -> Result<VecDeque<Parser>, OLRError>;
+    fn get_sequence_from_file(&self, log_archive_format : &String, file : &PathBuf) -> Option<u64>;
 }
 
 struct ArchiveDiggerOffline {
     archive_log_format : String, 
     db_recovery_file_destination : String,
     context : String,
-    last_checked_day : String,
+    min_sequence : Option<u64>,
     mapping_fn : Box<dyn Fn(PathBuf) -> PathBuf>,
 }
 
 impl ArchiveDiggerOffline {
     pub fn new(archive_log_format : String, db_recovery_file_destination : String,
-        context : String, last_checked_day : String, mapping_fn : Box<dyn Fn(PathBuf) -> PathBuf>) -> Self {
+        context : String,  
+        min_sequence : Option<u64>, mapping_fn : Box<dyn Fn(PathBuf) -> PathBuf>) -> Self {
         Self {
             archive_log_format, 
             db_recovery_file_destination,
             context,
-            last_checked_day,
+            min_sequence,
             mapping_fn,
         }
     }
@@ -52,23 +55,120 @@ impl ArchiveDigger for ArchiveDiggerOffline {
         let directory = mapped_path.read_dir()
             .or(olr_err!(MissingDir, "Can not read directory: {}", mapped_path.display()).into())?;
 
-        let parser_queue = VecDeque::new();
+        let mut parser_queue = VecDeque::new();
 
         for object in directory {
+            if let Err(err) = object {
+                warn!("Error while iterate the directory: {} error: {}", mapped_path.display(), err.to_string());
+                continue;
+            }
+
             let (object_path, object_type) = match object {
                 Ok(entry) => {
                     (entry.path(), entry.file_type())
                 },
-                Err(err) => {
-                    warn!("Error while iterate the dirrectory: {} error: {}", mapped_path.display(), err.to_string());
-                    continue;
-                }
+                _ => std::unreachable!()
             };
 
-            info!("Check path: {} type: {:?}", object_path.display(), object_type);
+            if let Err(err) = object_type {
+                warn!("Error while getting metadata of file: {} error: {}", object_path.display(), err.to_string());
+                continue;
+            }
+
+            let file_type = unsafe { object_type.unwrap_unchecked() };
+
+            if !file_type.is_dir() {
+                continue;
+            }
+
+            info!("Check path: {}", object_path.display());
+
+            let directory = object_path.read_dir()
+                .or(olr_err!(MissingDir, "Can not read directory: {}", object_path.display()).into())?;
+
+            for archive_file in directory {
+                if let Err(err) = archive_file {
+                    warn!("Error while iterate the directory: {} error: {}", object_path.display(), err.to_string());
+                    continue;
+                }
+                let archive_file = unsafe { archive_file.ok().unwrap_unchecked().path() };
+
+                let sequence = self.get_sequence_from_file(&self.archive_log_format, &archive_file);
+                
+                if sequence.is_none() {
+                    warn!("Bad sequence parsing of file: {}", archive_file.display());
+                    continue;
+                }
+
+                let sequence = unsafe { sequence.unwrap_unchecked() };
+
+                if self.min_sequence.is_some() && sequence < self.min_sequence.unwrap() {
+                    info!("Skip sequence {}", sequence);
+                    continue;
+                }
+
+                info!("Found sequence: {:?}", sequence);
+
+                parser_queue.push_back(Parser {  });
+            }
         }
 
         Ok(parser_queue)
+    }
+
+    fn get_sequence_from_file(&self, log_archive_format : &String, file : &PathBuf) -> Option<u64> {
+        let log_archive_format = log_archive_format.as_bytes();
+        let binding = file.file_name().unwrap().to_str().unwrap().to_string();
+        let file = binding.as_bytes();
+        
+        let mut sequence : u64 = 0;
+        let mut i = 0;
+        let mut j = 0;
+
+        while i < log_archive_format.len() && j < file.len() {
+            if log_archive_format[i] == b'%' {
+                if i + 1 >= log_archive_format.len() {
+                    return None;
+                }
+                let mut digits = 0;
+                if "strdSTa".as_bytes().contains(&log_archive_format[i + 1]) {
+                    // Some [0-9]*
+                    let mut number: u64 = 0;
+                    while j < file.len() && file[j] >= b'0' && file[j] <= b'9' {
+                        number = number * 10 + (file[j] - b'0') as u64;
+                        j += 1;
+                        digits += 1;
+                    }
+
+                    if log_archive_format[i + 1] == b's' || log_archive_format[i + 1] == b'S' {
+                        sequence = number;
+                    }
+                    i += 2;
+                } else if log_archive_format[i + 1] == b'h' {
+                    // Some [0-9a-z]*
+                    while j < file.len() && ((file[j] >= b'0' && file[j] <= b'9') || (file[j] >= b'a' && file[j] <= b'z')) {
+                        j += 1;
+                        digits += 1;
+                    }
+                    i += 2;
+                }
+
+                if digits == 0 {
+                    return None;
+                }
+            } else if file[j] == log_archive_format[i] {
+                i += 1;
+                j += 1;
+            } else {
+                return None;
+            }
+        }
+
+        if i == log_archive_format.len() && j == file.len() {
+            return Some(sequence);
+        }
+
+        return None;
     }
 }
 
@@ -78,16 +178,17 @@ mod tests {
     use crate::{common::errors::OLRError, init_logger};
     use super::{ArchiveDigger, ArchiveDiggerOffline};
 
-    fn create_offline_digger() -> ArchiveDiggerOffline {
+    fn create_offline_digger(min_seq : u64) -> ArchiveDiggerOffline {
         ArchiveDiggerOffline::new(
             "o1_mf_%t_%s_%h_.arc".to_string(),
             "".to_string(),
             "DB_NAME".to_string(),
-            "".to_string(),
+            Some(min_seq),
             Box::new(|path| -> PathBuf {
                 match path.to_str().unwrap() {
                     r"/opt/oracle/fst/archivelog" => r"/data/d2/archivelog".into(),
                     r"DB_NAME\archivelog" => r"./archivelog".into(),
+                    r"DB_NAME/archivelog" => r"./archivelog".into(),
                     _ => path,
                 }
             }),
@@ -96,14 +197,14 @@ mod tests {
 
     #[test]
     fn test_mapping_none() -> Result<(), OLRError> {
-        let digger = create_offline_digger();
+        let digger = create_offline_digger(0);
         assert_eq!((digger.mapping_fn)("".into()), PathBuf::from_str("").unwrap());
         Ok(())
     }
 
     #[test]
     fn test_mapping_ok() -> Result<(), OLRError> {
-        let digger = create_offline_digger();
+        let digger = create_offline_digger(0);
         assert_eq!((digger.mapping_fn)("/opt/oracle/fst/archivelog".into()), PathBuf::from_str("/data/d2/archivelog").unwrap());
         Ok(())
     }
@@ -111,9 +212,12 @@ mod tests {
     #[test]
     fn test_queue_getting() -> Result<(), OLRError> {
         init_logger();
-        let digger = create_offline_digger();
-        
-        let _ = digger.get_parsers_queue()?;
+        let q1 = create_offline_digger(0).get_parsers_queue()?;
+        assert_eq!(q1.len(), 8);
+
+
+        let q2 = create_offline_digger(173).get_parsers_queue()?;
+        assert_eq!(q2.len(), 3);
 
         Ok(())
     }
